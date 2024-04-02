0180b05fb2e0b78b9d2773bb87fcc5440ca20708
rdziewietin <rdzie14@gmail.com>
2024-04-02 19:25:06 -0400

    content incomplete

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -3,14 +3,13 @@ let tabs = document.getElementsByClassName("tab");
 for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    let content = this.nextElementSibling;
+    let content = this.parentElement.nextElementSibling;
     console.log(content.innerHTML);
-    // if (content.style.maxHeight) {
-    //   content.style.maxHeight = null;
-    //   content.style.padding = null;
-    // } else {
-    //   content.style.maxHeight = content.scrollHeight + "px";
-    //   content.style.padding = 8 + "px";
-    // }
+    if (content.style.display === "none"){
+      content.style.display = "block";
+    }
+    else {
+      content.style.display = "none";      
+    }
   });
 }
