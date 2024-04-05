3a12ebc39bc0583cb748a2b77f0f61308b5665d2
rdziewietin <rdzie14@gmail.com>
2024-04-04 23:44:46 -0400

    content complete

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,13 +1,12 @@
 let tabs = document.getElementsByClassName("tab");
 
 for (let i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", function() {
+  tabs[i].addEventListener("click", function () {
     let content = this.parentElement.nextElementSibling;
-    if (content.style.display === "block"){
+    if (content.style.display === "block") {
       this.innerHTML = "Expand";
       content.style.display = "none";
-    }
-    else {  
+    } else {
       this.innerHTML = "Collapse";
       content.style.display = "block";
     }
